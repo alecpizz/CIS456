7e8d976afa95471e5e5bb5bee668e411f245c3ea
Josh <jlovell@mail.bradley.edu>
2025-04-02 18:40:22 -0500

    Components probably work again

--- Game/src/Galaga/Galaga.cpp
+++ Game/src/Galaga/Galaga.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Galaga.h"
+#include "Components.h"
 
 namespace Galaga
 {
@@ -17,12 +18,22 @@ namespace Galaga
         get_event_manager()->window_event_logging(true);
         get_event_manager()->add_on_app_closing_event_listener(this);
 
-        _starfighter_system = std::make_unique<PlayerSystem>(this);
+        get_component_manager()->register_component<Transform2DComponent>();
+        get_component_manager()->register_component<RigidBody2DComponent>();
+        get_component_manager()->register_component<ColorComponent>();
+        get_component_manager()->register_component<TorqueComponent>();
+        get_component_manager()->register_component<SpriteComponent>();
+        get_component_manager()->register_component<PlayerComponent>();
+        get_component_manager()->register_component<BoundingBoxComponent>();
+        get_component_manager()->register_component<LifetimeComponent>();
+        get_component_manager()->register_component<DestructionNotificationComponent>();
 
-        get_system_manager()->register_system<PlayerComponent,
-            SpriteComponent, Transform2DComponent, RigidBody2DComponent>(*_starfighter_system);
+        //_starfighter_system = std::make_unique<StarfighterSystem>(this);
 
-        _starfighter_system->initialize();
+        //get_system_manager()->register_system<PlayerComponent,
+        //    SpriteComponent, Transform2DComponent, RigidBody2DComponent>(*_starfighter_system);
+
+        //_starfighter_system->initialize();
     }
 
     void Galaga::on_app_closing()
