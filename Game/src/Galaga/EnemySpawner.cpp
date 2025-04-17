4be872f97152225f3fe68b8040133a9f57b8f318
Josh <jlovell@mail.bradley.edu>
2025-04-17 16:56:20 -0500

    Player sprites are jank

--- Game/src/Galaga/EnemySpawner.cpp
+++ Game/src/Galaga/EnemySpawner.cpp
@@ -75,9 +75,9 @@ namespace Galaga
     {
         _enemy_entity = _game->get_entity_manager()->add_entity(Galaga::EntityType::Enemy);
         _game->get_component_manager()->add_component(*_enemy_entity, EnemyComponent{});
-        _game->get_component_manager()->add_component(*_enemy_entity, SpriteComponent{
+        //_game->get_component_manager()->add_component(*_enemy_entity, SpriteComponent{
             //.sprite = _enemy_sprites["#"].get()
-            });
+        //    });
         _game->get_component_manager()->add_component(*_enemy_entity, RigidBody2DComponent{
             .velocity = { _rands.get_uniform_real("enemy_velocity") * VELOCITY_ENEMY,
                         _rands.get_uniform_real("enemy_velocity") * VELOCITY_ENEMY }
